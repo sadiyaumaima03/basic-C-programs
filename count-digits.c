3aab0ac034a52e8e2d60da11787cc1b88601b868
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-05-01 00:34:59 +0530

    Update count-digits.c

--- count-digits.c
+++ count-digits.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int n; 
-    int count=0;
+    int n, count=0;
     printf("Enter a number:\n");
     scanf("%d",&n);
     
-    for(int i=0;i<4;i++){
-        int c;
-        c=n/10;
+    for(int i=n; i != 0; i/=10){
         count++;
     }
     
     printf("%d",count);
-    
-
     return 0;
 }
