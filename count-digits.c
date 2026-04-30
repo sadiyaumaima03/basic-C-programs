844cb47f0dfb48d1d3919255c91be10c1a8af0d3
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-04-30 23:59:05 +0530

    count_digits

---
+++ count-digits.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int n; 
+    int count=0;
+    printf("Enter a number:\n");
+    scanf("%d",&n);
+    
+    for(int i=0;i;i++){
+        int c;
+        c=n/10;
+        count++;
+    }
+    
+    printf("%d",count);
+    
+
+    return 0;
+}
