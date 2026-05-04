94395604ca512b70f4498f5154afec8a38c4f3c0
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-05-04 20:18:36 +0530

    floyd's_triangle

---
+++ floydtri.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+    int main(){
+        int n, count = 1;
+
+        printf("Enter a number:");
+        scanf("%d",&n);
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                printf("%d ", count);
+                count++;
+            }
+            printf("\n");
+        }
+        return 0;
+    }
