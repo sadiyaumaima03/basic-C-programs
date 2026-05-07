d62d8d15312d82ac7ef49c19e87fd697010db959
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-05-08 02:00:33 +0530

    pow()_square

---
+++ pow.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+    int main(){
+        int n;
+
+        printf("Enter a number:");
+        scanf("%d",&n);
+
+        printf("%.2f\n",pow(n,2));
+    return 0;
+    }
