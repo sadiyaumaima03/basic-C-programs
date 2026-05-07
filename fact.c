55020d9ec9d39bb1b3dd114164ae18ad12796e87
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-05-08 03:42:40 +0530

    factorial_using_recursion

---
+++ fact.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int fact(int n);
+
+    int main(){
+        printf("Factorial: %d",fact(5));
+    return 0;
+    }
+
+    int fact(int n){
+        if(n == 0){
+            return 1;
+        }
+    int factN_1 = fact(n-1);
+    int factN = factN_1 * n;
+    return factN;
+    }
