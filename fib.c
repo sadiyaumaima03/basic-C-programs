80f3edff77f35e2d82701db61e81a83ff86fed83
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-05-09 02:02:05 +0530

    fibonacci_using_recursion

---
+++ fib.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int fibonacci(int n);
+
+    int main(){
+
+        int n;
+        printf("Enter a number:");
+        scanf("%d",&n);
+        fibonacci(n);
+
+    return 0;
+    }
+
+int fibonacci(int n){
+    if(n == 0){
+        return 0;
+    } if(n == 1){
+        return 1;
+    }
+    int fibN_1 = fibonacci(n - 1);
+    int fibN_2 = fibonacci(n - 2);
+    int fibN = fibN_1 + fibN_2;
+    printf("Fibonacci of %d : %d\n",n,fibN);
+    return fibN;
+}
