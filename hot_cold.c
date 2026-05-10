3145c1e2ce58f40c9335bb019f61e925fedde35f
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-05-11 02:01:11 +0530

    Update hot_cold.c

--- hot_cold.c
+++ hot_cold.c
@@ -16,7 +16,7 @@ void hotcold(int temp){
     if(temp <= 0){
         printf("Cold weather\n");
     } else if(temp < 30){
-        printf("moderate weather\n");
+        printf("Moderate weather\n");
     } else if (temp >= 30){
         printf("Hot weather\n");
     }
