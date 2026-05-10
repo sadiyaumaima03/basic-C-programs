1ed69da9abd8e7623d553d6d4c49edba7f043d7c
Umaima Sadiya <sadiyaumaima8@gmail.com>
2026-05-11 01:55:51 +0530

    weather_temperature

---
+++ hot_cold.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+    
+void hotcold(int temp);
+
+    int main(){
+        
+        int temp;
+        printf("Enter temperature in celsius:");
+        scanf("%d",&temp);
+        hotcold(temp);
+
+    return 0;
+    }
+
+void hotcold(int temp){
+    if(temp <= 0){
+        printf("Cold weather\n");
+    } else if(temp < 30){
+        printf("moderate weather\n");
+    } else if (temp >= 30){
+        printf("Hot weather\n");
+    }
+    return; 
+}
